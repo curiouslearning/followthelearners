b40eb6352fc2b503a2c111a0a272a29c6a30aa63
George Dzavashvili <dzavashviligeorge@gmail.com>
2020-05-01 19:06:04 +0400

    Cluster click & get donor donations

--- ftl_donation_test/static/js/summary.js
+++ ftl_donation_test/static/js/summary.js
@@ -22,13 +22,38 @@ function InitializeMaps()
     var url = new URL(url_string);
     var paramValue = url.searchParams.get('[email]');
     console.log("found email: ", paramValue);
-    $(document).ready(function ()
-    {
-        $.get("/viewData", { email: paramValue}, function (data, status)
-        {
-            DisplayClusteredData(data.locations);
-        });
-    });
+    DisplayClusteredData([
+        {lat: -31.563910, lng: 147.154312},
+        {lat: -33.718234, lng: 150.363181},
+        {lat: -33.727111, lng: 150.371124},
+        {lat: -33.848588, lng: 151.209834},
+        {lat: -33.851702, lng: 151.216968},
+        {lat: -34.671264, lng: 150.863657},
+        {lat: -35.304724, lng: 148.662905},
+        {lat: -36.817685, lng: 175.699196},
+        {lat: -36.828611, lng: 175.790222},
+        {lat: -37.750000, lng: 145.116667},
+        {lat: -37.759859, lng: 145.128708},
+        {lat: -37.765015, lng: 145.133858},
+        {lat: -37.770104, lng: 145.143299},
+        {lat: -37.773700, lng: 145.145187},
+        {lat: -37.774785, lng: 145.137978},
+        {lat: -37.819616, lng: 144.968119},
+        {lat: -38.330766, lng: 144.695692},
+        {lat: -39.927193, lng: 175.053218},
+        {lat: -41.330162, lng: 174.865694},
+        {lat: -42.734358, lng: 147.439506},
+        {lat: -42.734358, lng: 147.501315},
+        {lat: -42.735258, lng: 147.438000},
+        {lat: -43.999792, lng: 170.463352}
+      ]);
+    // $(document).ready(function ()
+    // {
+    //     $.get("/viewData", { email: paramValue}, function (data, status)
+    //     {
+    //         DisplayClusteredData(data.locations);
+    //     });
+    // });
 }
 
 /**
@@ -43,7 +68,7 @@ function DisplayClusteredData(locationData)
 
     var markers = locationData.map(function (location, i)
     {
-        let sanitizedLocation = { lat: location._latitude, lng: location._longitude };
+        let sanitizedLocation = { lat: location.lat, lng: location.lng };
         var newMarker = new google.maps.Marker({ position: sanitizedLocation });
         bounds.extend(newMarker.position);
 
@@ -64,11 +89,35 @@ function DisplayClusteredData(locationData)
 
     var markerCluster = new MarkerClusterer(mapRef, markers,
     {
-        imagePath: '/static/imgs/'
+        imagePath: '/static/imgs/',
+        zoomOnClick: false
+    });
+
+    markerCluster.addListener("clusterclick", function()
+    {
+        var currentCluster = markerCluster.getMarkers();
+        console.log(currentCluster);
+        if (currentCluster.length === 0)
+        {
+            console.log("There are no markers in this cluster, how did we get here.");
+        }
+        else
+        {
+            var firstMarker = currentCluster[0];
+            // mapsSharedInfoWindow.setContent(constructInfoWindowContent(
+            //     "Australia",
+            //     "Sydney",
+            //     "Random Fact. Random Fact. Random Fact. Random Fact. Random Fact. Random Fact. Random Fact.",
+            //     sanitizedLocation.lat,
+            //     sanitizedLocation.lng,
+            //     180));
+            // mapsSharedInfoWindow.open(mapRef, newMarker);
+        }
     });
 
     mapRef.fitBounds(bounds);
-    mapRef.setZoom(mapZoomLevel);
+    mapRef.panToBounds(bounds);
+    // mapRef.setZoom(mapZoomLevel);
 }
 
 function constructInfoWindowContent(country, region, randomFact, latitude, longitude, heading)
