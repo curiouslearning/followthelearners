ef69a3fb1c04e570396dff64b89d847310288dcf
George Dzavashvili <dzavashviligeorge@gmail.com>
2020-07-24 11:41:59 +0400

    Fix formatting for try catch block

--- static/js/index.js
+++ static/js/index.js
@@ -443,18 +443,16 @@ async function displayAllLearnersData(locData, isCountryLevelData, country) {
               label: { text: learnerCount.toString() }});
   
           try {
-		  regionMarker['lat'] = firstStreetViewLoc._latitude;
-		  regionMarker['lng'] = firstStreetViewLoc._longitude;
-		  regionMarker['country'] = country;
-		  regionMarker['facts'] = countryData.facts;
-		  regionMarker['region'] = region.region;
-		  regionMarker['heading'] = region.streetViews.headingValues[0];
-		  regionMarker['otherViews'] = [];
-	  } catch(e) {
-		  console.error("caught error: ",e," on region: ", region.region," in country: ", country);
-	  }
-
-          
+            regionMarker['lat'] = firstStreetViewLoc._latitude;
+            regionMarker['lng'] = firstStreetViewLoc._longitude;
+            regionMarker['country'] = country;
+            regionMarker['facts'] = countryData.facts;
+            regionMarker['region'] = region.region;
+            regionMarker['heading'] = region.streetViews.headingValues[0];
+            regionMarker['otherViews'] = [];
+          } catch(e) {
+            console.error("caught error: ",e," on region: ", region.region," in country: ", country);
+          }
           if (region.streetViews.locations.length > 1 &&
               region.streetViews.locations.length === 
               region.streetViews.headingValues.length) {
