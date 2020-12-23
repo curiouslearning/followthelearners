2fa5b1925af74192a73b491890a8e6be159e3063
George Dzavashvili <dzavashviligeorge@gmail.com>
2020-12-23 10:39:40 +0400

    Add index ts with tab toggle

--- static/js/index.ts
+++ static/js/index.ts
@@ -1,1 +1,27 @@
-console.log('Meow');
+import { TabSelector } from "./tabSelector";
+import { Config } from "./config";
+
+export class App {
+  private config: Config;
+  public TabSelector: TabSelector;
+
+  constructor() {
+    this.config = new Config();
+    this.TabSelector = new TabSelector(this.config);
+    this.init();
+  }
+
+  init(): void {
+    if (!this.TabSelector.isInitialized()) {
+      return;
+    }
+    this.TabSelector.addEventListener('preTabToggle', (tabId: string) => {
+      console.log(`WOW TOGGLING TAB: ${tabId}`);
+    });
+  }
+}
+
+/* Initialization */
+window.onload = () => {
+  let app = new App();
+}
