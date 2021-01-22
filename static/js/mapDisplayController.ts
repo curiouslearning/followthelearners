2b2a25ce184fd1f3d27c8c5559a62aa5c49abb41
George Dzavashvili <dzavashviligeorge@gmail.com>
2021-01-22 15:59:30 +0400

    Increase the size of info window close button

--- static/js/mapDisplayController.ts
+++ static/js/mapDisplayController.ts
@@ -425,6 +425,13 @@ export class MapDisplayController {
     this.infoWindow?.setContent(this.compileInfoWindowContent(marker));
     this.infoWindow?.open(this.map!);
     this.infoWindow?.setPosition({lat: marker.get('lat'), lng: marker.get('lng')});
+    let closeButtons: HTMLCollection = Helpers.getElement('.gm-ui-hover-effect') as HTMLCollection;
+    if (closeButtons.length > 0) {
+      let imgElement = closeButtons[0].children[0] as HTMLElement;
+      imgElement.style.width = '20px';
+      imgElement.style.height = '20px';
+      imgElement.style.margin = '10px 10px 0px 0px';
+    }
   }
 
   /**
