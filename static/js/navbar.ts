b5d4a3a723b25740285a023f02f09cdcd12161ee
George Dzavashvili <dzavashviligeorge@gmail.com>
2021-01-28 00:14:45 +0400

    Rename navbar scroll to navbar

--- static/js/navbar.ts
+++ static/js/navbar.ts
@@ -4,15 +4,20 @@ import { Helpers } from "./helpers";
 /**
  * Navbar Scroll class enables the navbar to change size when scrolling the page
  */
-export class NavbarScroll {
+export class Navbar {
   protected config: Config;
 
   private navbarId: string = '';
   private navbar: HTMLElement | null = null;
+  private mailchimpButtonId: string = '';
+  private mailchimpButton : HTMLButtonElement | null = null;
+
+  public mailchimpButtonClickCallback : { (): void } = () => {};
 
   constructor(config: Config) {
     this.config = config;
     this.navbarId = this.config.navbarId;
+    this.mailchimpButtonId = this.config.mailchimpButtonId;
   }
 
   /**
@@ -21,6 +26,14 @@ export class NavbarScroll {
   public init(): void {
     this.navbar = this.navbarId === '' ? null :
       Helpers.getElement(this.navbarId) as HTMLElement;
+    this.mailchimpButton = this.mailchimpButtonId === '' ? null :
+      Helpers.getElement(this.mailchimpButtonId) as HTMLButtonElement;
+
+    if (this.mailchimpButton) {
+      this.mailchimpButton.addEventListener('click', () => {
+        this.mailchimpButtonClickCallback();
+      });
+    }
 
     window.onscroll = () => {
       if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
@@ -31,5 +44,9 @@ export class NavbarScroll {
         this.navbar!.style.maxHeight = '120px';
       }
     };
+  }
+
+  public setMailChimpButtonClickListener(callback: { (): void }) {
+    this.mailchimpButtonClickCallback = callback;
   }
 }
